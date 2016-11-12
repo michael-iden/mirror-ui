28e01667c72a5be8f25e37566c0e473457a12c5a
Michael Iden <michaeliden90@gmail.com>
2016-11-12 11:50:44 -0500

    Removing other env files

--- src/environments/environment.dev.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApiKey: 'a221452dd0defa5b'
-};
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiKey: 'a221452dd0defa5b'
-};
